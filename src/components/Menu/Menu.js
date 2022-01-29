01a1e85538cb270aef51cbb99b9dcce8667897bb
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2022-01-29 13:57:46 +0300

    Fix: header fixed, animation menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -4,14 +4,16 @@ import './Menu.css';
 function Menu({ header, items, active }) {
   return (
     <div className={active ? 'menu active' : 'menu'}>
-      <h4 className="menu__header">{header}</h4>
-      <ul className="menu__lists">
-        {items.map((item) => (
-          <li key={item.href}>
-            <NavLink to={item.href}>{item.value}</NavLink>
-          </li>
-        ))}
-      </ul>
+      <div className="menu__block">
+        <h4 className="menu__header">{header}</h4>
+        <ul className="menu__lists">
+          {items.map((item) => (
+            <NavLink className="menu__link" key={item.href} to={item.href}>
+              {item.value}
+            </NavLink>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
