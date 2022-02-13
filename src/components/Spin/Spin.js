720fa9540bd22f550a6c6bf79555648410696844
Evgeniy Sergeev <akaevgeniy@yandex.ru>
2022-02-13 19:19:47 +0300

    Fix: add spin styles

--- src/components/Spin/Spin.js
+++ src/components/Spin/Spin.js
@@ -1,11 +1,12 @@
 import { useSelector } from 'react-redux';
 import { BallTriangle } from 'react-loader-spinner';
+import styles from './Spin.module.css';
 
 const Spin = () => {
   const spinner = useSelector((state) => state.appReducer.loading);
 
   return (
-    <div className="loader-styles">
+    <div className={styles.loader}>
       <BallTriangle color="#4169E1" height={80} width={80} visible={spinner} />
     </div>
   );
